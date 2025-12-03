bd10a74db18407c6917172c567cba92083b57b29
Jessicah Rhebeccah <jessicah.rhebeccah@gmail.com>
2025-12-03 16:48:47 -0300

    Send email subtask completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  email = 'jrkgs@cesar.org.br';
+  subject = 'Contato';
+  body = 'Gostaria de tirar uma dúvida';
+  url = `mailto:${this.email}?subject=${this.subject}&body=${this.body}`;
+
+  emailMe() {
+    window.open(this.url);
+  }
+
+  ngOnInit() {}
 }
